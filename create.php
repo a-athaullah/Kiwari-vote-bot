c9394aaa57830eac102dc78868070f55e6866e03
Ahmad Athaullah <ahmad.athaullah@gmail.com>
2019-02-13 10:40:03 +0700

    connect create vote process to postgres

--- create.php
+++ create.php
@@ -1,5 +1,19 @@
 <?php 
-$db = new SQLite3('vote.db');
+$db_host = "ec2-54-235-159-101.compute-1.amazonaws.com";
+$db_name = "d3poplfkcj6hve";
+$db_user = "lbhprhtzuemwyg";
+$db_pass = "531f614bf06282f8d96c6fab44a11876bdd060ec091c71af5fd6b124d534f6b0";
+$db_port = "5432";
+
+$db_conn_string = "host=".$db_host." port=".$db_port." dbname=".$db_name." user=".$db_user." password=".$db_pass;
+
+$db = pg_connect($db_conn_string);
+
+if (!$db) {
+    echo "An DB error occurred.\n";
+    exit;
+}
+
 
 $QISCUS_APP_ID = "kiwari-prod";
 $QISCUS_SDK_SECRET = "kiwari-prod-123";
@@ -12,9 +26,12 @@ $voteTitle = $_POST["title"];
 $voteDesc = $_POST["desc"];
 
 // print_r($_POST);
-$db->exec("INSERT INTO vote (title,body,roomId) VALUES ('$voteTitle','$voteDesc','$roomId');");
-$voteId = $db->lastInsertRowID();
+$voteQuery = "INSERT INTO vote (title,body,roomId) VALUES ('$voteTitle','$voteDesc','$roomId') RETURNING id;";
+$voteQueryExec = pg_query($db,$voteQuery); 
+$voteRow = pg_fetch_row($voteQueryExec); 
+$voteId = $voteRow['0'];
 
+error_log(print_r("new voteId: ".$voteId, TRUE));
 
 $message = array();
 $message['token'] = $bot_token;
@@ -29,8 +46,11 @@ $data['url'] = "https://web.kiwari.id";
 $data['buttons'] = array();
 
 foreach ($_POST["option"] as $voteItem){
-    $db->exec("INSERT INTO vote_item (vote_id,value) VALUES ('$voteId','$voteItem');");
-    $optId = $db->lastInsertRowID();
+    $optQuery = "INSERT INTO vote_item (vote_id,value) VALUES ('$voteId','$voteItem') RETURNING id;";
+    $optQueryExec = pg_query($db,$optQuery); 
+    $optRow = pg_fetch_row($optQueryExec); 
+    $optId = $voteRow['0'];
+
     $button = array();
     $button['label'] = "VBV.".$optId." ".$voteItem;
     $button['type'] = "postback";
@@ -57,10 +77,9 @@ $context = stream_context_create(array(
 $response = file_get_contents($url, FALSE, $context);
 error_log(print_r($response, TRUE));
 
-// var_dump($json);
+pg_close($db);
 
 print("Poll created, please back to your room.")
-
 
 
 ?>
